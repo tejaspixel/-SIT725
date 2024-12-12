4a681e3d37f711e366a3ddda1c15dd695066f9e4
tejaspixel <tru78patel@gmail.com>
2024-12-12 22:23:28 +1100

    Delete 5.2C/5.2C directory

--- 5.2C/5.2C/routes/blogRoutes.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-const router = express.Router();
-import blogController from '../controllers/blogController.js';
-
-router.get('/', blogController.getBlogs);
-
-export default router;
